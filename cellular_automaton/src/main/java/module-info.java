ba2c1310f513e7016f9414e2f29b3cd8b76175fa
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2024-07-31 15:41:27 +0700

    [cellular automaton]: added javafx.controls to module-info

--- cellular_automaton/src/main/java/module-info.java
+++ cellular_automaton/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module cellular_automaton {
     requires javafx.base;
     requires javafx.graphics;
+    requires javafx.controls;
 
     exports hibiscus3000.cellular_automaton.view to javafx.graphics;
 }
