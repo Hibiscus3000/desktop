3163f8a03cfb0a363e83ebd2a60a394a4c6f07f1
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2025-07-20 18:32:05 +0700

    [tetris]: Fix stop

--- tetris/src/main/java/hibiscus3000/tetris/model/manager/GameManager.java
+++ tetris/src/main/java/hibiscus3000/tetris/model/manager/GameManager.java
@@ -52,6 +52,7 @@ public class GameManager implements GameListener, AutoCloseable {
         stopGame();
         field.clear();
         figure = null;
+        notifyRemoval();
         gameLost = false;
     }
 
